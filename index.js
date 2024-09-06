57d35b651a9bc464dfefbd1f50d0357799eea214
DolzhenkoS <sergey_dolzhenko@gmail.com>
2024-09-06 14:36:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
-let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
+//let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
 
-tg.expand(); //расширяем на все окно  
+//tg.expand(); //расширяем на все окно  
 
- tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
- tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
- tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
- tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
- tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+ //tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+ //tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+//  tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+//  tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+//  tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
 
 // btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
 // 	if (tg.MainButton.isVisible){ //если кнопка показана 
@@ -51,6 +51,7 @@ tg.expand(); //расширяем на все окно
 
  let profName = document.createElement('p'); //создаем параграф
  profName.innerText = "PARAGRAPH";
+ document.appendChild(profName);
 
 // let profName = document.createElement('p'); //создаем параграф
 // profName.innerText = `${tg.initDataUnsafe.user.first_name}
